e0817a2b3665dd4eb1607769651c5b6bb4a9ee73
Ajmal Muhammad P <ajumalp@gmail.com>
2021-05-09 22:47:05 +0530

    initial check-in

---
+++ example/android/app/src/main/kotlin/com/example/es_utils_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.es.es_utils_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/es_utils.dart
@@ -0,0 +1,8 @@
+/*
+ * Developed by Ajmal Muhammad P
+ * Contact me @ support@erratums.com
+ * https://erratums.com
+ * Date created: 09-May-2021
+ */
+
+library es_utils;
