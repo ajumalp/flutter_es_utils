dd32c7dd16afef30001f1cb51fbbeac931337637
Ajmal Muhammad P <ajumalp@gmail.com>
2022-04-14 14:31:19 +0530

    Added capitalizeFirstChar

--- lib/es_base_extensions.dart
+++ lib/es_base_extensions.dart
@@ -5,6 +5,29 @@
  * Date created: 23-July-2021
  */
 
+extension ESStringExtn on String {
+  String get capitalizeFirstChar {
+    switch (length) {
+      case 0:
+        return '';
+
+      case 1:
+        return this[0].toUpperCase();
+
+      default:
+        return this[0].toUpperCase() + substring(1);
+    }
+  }
+
+  String get capitalizeFirstCharOfWords {
+    if (length < 1) return '';
+
+    String result = '';
+    split(' ').forEach((word) => result += word.capitalizeFirstChar);
+    return result;
+  }
+}
+
 extension ESDoubleEx on double {
   String toStringAsFixedNoZero(final int n) => double.parse(toStringAsFixed(n)).toString();
 }
