588b626e3e6d67c8b69ab94966a90f03669816e2
Ajmal Muhammad P <ajumalp@gmail.com>
2022-07-15 16:43:38 +0530

    Added few utils functions

--- lib/es_utils.dart
+++ lib/es_utils.dart
@@ -8,6 +8,8 @@
 library es_utils;
 
 import 'dart:math';
+import 'package:es_utils/es_utils.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as httpClient;
 
 export 'es_base_extensions.dart';
@@ -25,4 +27,102 @@ class ESUtils {
   }
 
   static Future<String> readFile(final String url) => httpClient.read(Uri.parse(url));
+
+  static pushScreen({
+    required BuildContext context,
+    required Widget newScreen,
+    bool popRequired = true,
+    bool showMiniWindow = false,
+    BoxConstraints? constraints,
+    bool barrierDismissible = true,
+  }) {
+    if (popRequired) Navigator.of(context).pop();
+    if (showMiniWindow) {
+      ESMessage.showModalDialog(
+        context,
+        barrierDismissible: barrierDismissible,
+        content: ClipRRect(
+          borderRadius: ESPlatform.isSmallScreen(context) ? null : BorderRadius.circular(10),
+          clipBehavior: Clip.antiAlias,
+          child: newScreen,
+        ),
+        constraints: constraints,
+      );
+    } else {
+      Navigator.of(context).push(MaterialPageRoute(builder: (_) => newScreen));
+    }
+  }
+
+  static Scaffold scaffold(
+    BuildContext context, {
+    required String title,
+    Widget? body,
+    IconData? iconData,
+    List<Widget>? footerButtons,
+    final bool hideCloseButton = false,
+    List<Widget>? actions,
+    final Color? backgroundColor,
+  }) {
+    final bool isSmallScreen = ESPlatform.isSmallScreen(context);
+
+    if (!hideCloseButton && !isSmallScreen) {
+      (actions ??= []).add(FocusScope(
+        canRequestFocus: false,
+        child: IconButton(
+          tooltip: 'Close [Esc]',
+          icon: const Icon(Icons.close),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ));
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        title: Text(title),
+        automaticallyImplyLeading: isSmallScreen || iconData == null,
+        leading: isSmallScreen || iconData == null ? null : Icon(iconData),
+        titleSpacing: 0,
+        actions: actions,
+      ),
+      body: body,
+      persistentFooterButtons: footerButtons,
+    );
+  }
+
+  static Card insertInCard(
+    Widget widget, {
+    final EdgeInsetsGeometry childPadding = EdgeInsets.zero,
+    final bool includeBorderSide = true,
+    final Color useDarkTheme = Colors.white,
+  }) {
+    return Card(
+      color: useDarkTheme,
+      shadowColor: Colors.blueGrey,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      margin: childPadding,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+        side: includeBorderSide ? const BorderSide(color: Colors.blueGrey, width: 1.5) : BorderSide.none,
+      ),
+      child: widget,
+    );
+  }
+
+  static Widget createControlsInRow(List<Widget> aControls, {bool addBottomSpace = true, bool addSpaceAround = true}) {
+    final List<Widget> varResult = [];
+    for (var control in aControls) {
+      if (addSpaceAround && varResult.isNotEmpty) varResult.add(const SizedBox(width: 10));
+      varResult.add(Flexible(child: control));
+    }
+    if (addBottomSpace) varResult.add(const SizedBox(height: 10));
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: varResult,
+      ),
+    );
+  }
 }
